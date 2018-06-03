d6daa8b11aca928f5979039a9b3200a18440bb06
Ranjith <32501897+rjith12@users.noreply.github.com>
2018-06-03 19:54:55 -0400

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,19 @@
+public class Reverse {
+        public static void main(String[] args) {
+            int[] x = {0,1,2,3,4,5,6,7,8,9};
+            
+            System.out.println("Countdown");
+            System.out.println(x[9] + 1);
+            System.out.println(x[9]);
+            System.out.println(x[8]);
+            System.out.println(x[7]);
+            System.out.println(x[6]);
+            System.out.println(x[5]);
+            System.out.println(x[4]);
+            System.out.println(x[3]);
+            System.out.println(x[2]);
+            System.out.println(x[1]);
+            System.out.println(x[0]);
+        }
+
+    }
