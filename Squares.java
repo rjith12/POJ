b44f933487097ac4761b7af86aaeba4ed530aeef
Ranjith <32501897+rjith12@users.noreply.github.com>
2018-06-03 19:54:36 -0400

    Add files via upload

---
+++ Squares.java
@@ -0,0 +1,12 @@
+public class Squares {
+        public static void main(String[] args) {
+            int[] squares = {0,1,4,9,16};
+            
+            System.out.println(squares[0]);
+            System.out.println(squares[1]);
+            System.out.println(squares[2]);
+            System.out.println(squares[3]);
+            System.out.println(squares[4]);
+            }
+    
+}
